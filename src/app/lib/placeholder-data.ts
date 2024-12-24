9afa39c59ca27c8596e4e52597c7e60d6de5505b
SENG KOSAL <sengkosal023@gmail.com>
2024-12-24 11:00:34 +0700

    initial

--- src/app/lib/placeholder-data.ts
+++ src/app/lib/placeholder-data.ts
@@ -1,20 +1,20 @@
-const customers = [
-  { id: 1, name: "Customer A" },
-  { id: 2, name: "Customer B" },
-];
-///// from pu T
-const invoices = [
-  {
-    customer_id: customers[0].id,
-    amount: 15795,
-    status: "pending",
-    date: "2022-12-06",
-  },
-  {
-    customer_id: customers[1].id,
-    amount: 20348,
-    status: "pending",
-    date: "2022-11-14",
-  },
-  // ...
-];
+// const customers = [
+//   { id: 1, name: "Customer A" },
+//   { id: 2, name: "Customer B" },
+// ];
+// ///// from pu T
+// const invoices = [
+//   {
+//     customer_id: customers[0].id,
+//     amount: 15795,
+//     status: "pending",
+//     date: "2022-12-06",
+//   },
+//   {
+//     customer_id: customers[1].id,
+//     amount: 20348,
+//     status: "pending",
+//     date: "2022-11-14",
+//   },
+//   // ...
+// ];
