45d148e9689390cf7213de3071949626fdb2bfab
SENG KOSAL <sengkosal023@gmail.com>
2024-12-29 17:38:06 +0700

    Your commit message

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,40 @@
-export default function Page() {
-    return <p>Dashboard Pageeeeeee Home</p>;
-  }
+import CardWrapper from "@/app/ui/dashboard/cards";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { lusitana } from "@/app/ui/fonts";
+import { fetchLatestInvoices } from "@/app/lib/data";
+import { Suspense } from "react";
+import { RevenueChartSkeleton, CardSkeleton } from "@/app/ui/skeletons";
+
+export default async function Page() {
+  const latestInvoices = await fetchLatestInvoices();
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+
+
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* <Suspense fallback={<CardSkeleton />}>
+        </Suspense> */}
+          <CardWrapper />
+
+        <h3>Ti1</h3>
+        
+      </div>
+
+
+
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <h2>ti2</h2>
+        {/* <Suspense fallback={<RevenueChartSkeleton />}>
+        </Suspense> */}
+          <RevenueChart />
+          
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+    </main>
+  );
+}
